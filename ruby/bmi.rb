d3e25464263ab5b85907d304de7fcac79c4e3829
diceratops <diceratops@sadie.(none)>
2011-08-27 22:25:13 -0400

    added ruby group

---
+++ ruby/bmi.rb
@@ -0,0 +1,9 @@
+def bmi(w, h)
+	return (w / ( h * h )) * 703
+end
+puts "Please enter your weight"
+w = gets.to_f
+puts "Please enters your height in inches"
+h = gets.to_f
+puts "Your BMI is"
+puts bmi(w, h)
