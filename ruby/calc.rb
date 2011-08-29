d978246553617c11f75456a87411bf0d0756757d
diceratops <diceratops@sadie.(none)>
2011-08-29 10:48:44 -0400

    added simple calculation program

--- ruby/calc.rb
+++ ruby/calc.rb
@@ -1,10 +1,10 @@
 #hours in a year
 def hours
-	return  24*365
+	return  24 * 365
 end
 #minutes in a year
 def mins 
-	return (hours * 60)
+	return hours * 60
 end
 #your age in seconds
 def age
@@ -12,7 +12,7 @@ def age
 end
 
 puts 'There are ' + hours.to_s + 'hours  in a year'
-puts 'There are ' + (mins*10).to_s + ' Minutes in a decade'
+puts 'There are ' + (mins * 10 + (48*60)).to_s + ' minutes in a decade'
 puts 'It\'s been ' + age.to_s + ' seconds since i was born'
 
 #author's age
