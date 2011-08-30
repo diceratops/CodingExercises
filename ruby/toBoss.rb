e6de9e194b33037e325d452a281cf0bf6f53eacb
diceratops <diceratops@sadie.(none)>
2011-08-30 14:40:30 -0400

    boss is yelling at you program

--- ruby/toBoss.rb
+++ ruby/toBoss.rb
@@ -1,3 +1,3 @@
 puts 'Say something to your boss, go on then, Don\'t be shy.'
 toBoss = gets.chomp
-puts 'WHAT DO YOU MEAN!? ' + toBoss + '? YOU\'RE FIRED!'
+puts 'WHAT DO YOU MEAN!? ' + toBoss.upcase + '? YOU\'RE FIRED!'
