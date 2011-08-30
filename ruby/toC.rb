2c494b96d0a9526e1094ca9f0e843c632de868c8
diceratops <diceratops@sadie.(none)>
2011-08-30 14:41:01 -0400

    formatting exercise using justify/center/rightjustify/leftjustify

---
+++ ruby/toC.rb
@@ -0,0 +1,14 @@
+
+ch = 'Chapter '
+ch1 = 'Getting Started'
+ch2 = 'Numbers'
+ch3 = 'Letters'
+
+pg = 'page'
+
+puts 'Table of Contents'.center (60)
+puts ((ch + '1').ljust(15) + ch1.ljust(40) + pg.rjust(30) + '1'.rjust(3))
+puts ((ch + '2').ljust(15) + ch2.ljust(40) + pg.rjust(30) + '9'.rjust(3))
+puts ((ch + '3').ljust(15) + ch3.ljust(40) + pg.rjust(30) + '13'.rjust(3))
+
+
