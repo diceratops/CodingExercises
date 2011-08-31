fbba853aa55f3edf33b546c45f44c95d2f0a9dc4
diceratops <diceratops@sadie.(none)>
2011-08-31 19:35:16 -0400

    grandma doesn\'t want you to go away

--- ruby/grandma.rb
+++ ruby/grandma.rb
@@ -1,16 +1,21 @@
 puts 'Say something to your grandma, she\'s kinda hard of hearing through, FYI.'
-
+numBYE = 0 
 
 while true
-
-	convoStart = gets  #make sure this comes BEFORE the while, loop silly!
+	convoStart = gets.chomp  #make sure this comes BEFORE the while, loop silly!
+	if convoStart == "BYE" #you need the chomp method with gets to make this work
+		numBYE = numBYE + 1
+		if numBYE == 3
+			break
+		end
+	else
+		numBYE = 0
+	end
 
 	if convoStart == convoStart.downcase
 		puts 'HUH!? SPEAK UP, SONNY!'
-		
 	elsif convoStart == convoStart.upcase
-		puts 'NO! Not since ' + (rand(31) + 1930).to_s
-		break
+		puts 'NO! Not since ' + (rand(21) + 1930).to_s
 	end
 end
 
