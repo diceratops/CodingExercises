64ece3f37b475fff06c1a5f3a0b448ae468c735e
diceratops <diceratops@sadie.(none)>
2011-08-31 10:13:33 -0400

    used the join method to add spacing between names

--- ruby/greeting.rb
+++ ruby/greeting.rb
@@ -3,7 +3,13 @@ firstName = gets.chomp
 puts 'And what is your middle name?'
 middleName = gets.chomp
 puts 'And what is your last name, praytell?'
-lastName = gets.chomp
+lastName = gets.chomp.join
 
 puts ''
-puts 'Thanks ' + firstName + ' '  + middleName + ' ' + lastName + '. Pleased to meet you.'
+
+# puts 'Thanks ' + firstName + ' '  + middleName + ' ' + lastName + '. Pleased to meet you.'
+nameArry = ['Thanks', firstName, middleName, lastName, 'Pleased to meet you.'].join(' ')
+puts nameArry
+
+#how can i get a period after the last name, without compromising the (' ') argument
+#that i added with the join method?
