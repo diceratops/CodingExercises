9ab3d6d0c8e02f2e18f6d92f2bddc82a49ebf40f
diceratops <diceratops@sadie.(none)>
2011-08-31 19:41:39 -0400

    added period after name

--- ruby/greeting.rb
+++ ruby/greeting.rb
@@ -3,13 +3,11 @@ firstName = gets.chomp
 puts 'And what is your middle name?'
 middleName = gets.chomp
 puts 'And what is your last name, praytell?'
-lastName = gets.chomp.join
+lastName = gets.chomp
 
 puts ''
 
 # puts 'Thanks ' + firstName + ' '  + middleName + ' ' + lastName + '. Pleased to meet you.'
-nameArry = ['Thanks', firstName, middleName, lastName, 'Pleased to meet you.'].join(' ')
+nameArry = ['Thanks', firstName, middleName, lastName+'.', 'Pleased to meet you.'].join(' ')
 puts nameArry
 
-#how can i get a period after the last name, without compromising the (' ') argument
-#that i added with the join method?
