ec2421f1b10f4fbe0ab14e9a27c89fef67be99c9
diceratops <diceratops@sadie.(none)>
2011-08-31 11:30:02 -0400

    added 99 bottles, kept making the mistake of setting the minus 1 after the while, but before the change! wtf?

---
+++ ruby/99bottles.rb
@@ -0,0 +1,15 @@
+#99 Bottles of beer on the wall.
+puts 'How many bottles of beer are on the wall?'
+numStart = gets.to_i
+numBot = numStart
+
+
+while numBot > 2
+	puts numBot.to_s + ' bottes of beer on the wall ' + numBot.to_s + ' bottles of beer'
+	numBot = numBot - 1 
+	puts 'take one down, pass it around ' + numBot.to_s + ' bottles of beer on the wall'
+end
+puts '2 bottles of beer on the wall 2 bottles of beer, take one down'
+puts 'pass it around, 1 bottle of beer on the wall.'
+	
+
